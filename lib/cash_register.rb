881fe175c5c7c0fac6cc51f52ab0e626c00a66c5
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-21 10:50:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,18 @@
 class CashRegister 
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items, :previous_total 
   
   def initialize (discount = nil)
     @total = 0 
     @discount = discount
   end
   
-  def add_item (title, price, quantity)
+  def add_item (title, price, quantity=1)
+    @previous_total = total
+    @total += (price * quantity)
+    quantity.times.do |qty|
+      @items << title
+    end
+      
     
   end
 end
